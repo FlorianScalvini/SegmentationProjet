Fix issue training Change mIoU calculation Add transform

train.py
@@ -87,17 +87,17 @@ class Trainer():
             if isinstance(preds, tuple) or isinstance(preds, list):
                 preds = preds[0]
             pred = torch.argmax(preds, dim=1, keepdim=True).squeeze()
-            inter, pPred, pTarget = calculate_area(pred, target, preds.shape[1])
+            inter, pPred, pTarget = calculate_hist(pred, target, preds.shape[1], ignore_labels=self.ignore_labels)
             intersect = torch.add(inter, intersect)
             pred_area = torch.add(pPred, pred_area)
             label_area = torch.add(pTarget, label_area)
 
         # RETURN LOSS & METRICS
-        class_iou, miou = meanIoU(aInter=intersect, aPreds=pred_area, aLabels=label_area, ignore_label=self.ignore_labels)
+        class_iou, miou = meanIoU(aInter=intersect, aPreds=pred_area, aLabels=label_area)
         acc, class_precision, class_recall = class_measurement(aInter=intersect, aPreds=pred_area, aLabels=label_area)
         kap = kappa(aInter=intersect, aPreds=pred_area, aLabels=label_area)
         log = {
-            'loss': self.total_loss / (len(self.train_loader) * self.train_loader.batch_size),
+            'loss': self.total_loss / (len(self.train_loader) * self.train_loader.loader.batch_size),
             'miou': miou,
             'class_iou': class_iou,
             'class_precision': class_precision,
@@ -110,7 +110,7 @@ class Trainer():
         best_metric = 0
         for epoch in range(self.start_epoch, self.epochs + 1):
             train_log = self._train_epoch()
-            val_log = evaluate(model=self.model, eval_loader=self.val_loader, num_classes=self.val_loader.numberClasses, precision=self.precision, print_detail=False)
+            val_log = evaluate(model=self.model, eval_loader=self.val_loader, num_classes=self.val_loader.dataset.num_classes, precision=self.precision, print_detail=False)
             self.scheduler.step(val_log['loss'])
             # LOGGING INFO
             self.logger.info(f'\n ## Info for epoch {epoch} ## ')

transform.py
@@ -16,14 +16,16 @@ class Transform:
                 return
         self.transforms.append(ToTensor())
 
-    def __call__(self, im, label=None):
+    def __call__(self, image, label=None):
         for transform in self.transforms:
-            outputs = transform(im, label)
-            im = outputs[0]
-            if len(outputs) == 2:
-                label = outputs[1]
-
-        return (im, label)
+            if label is None:
+                image = transform(image, label)
+            else:
+                image, label = transform(image, label)
+        if label is None:
+            return image
+        else:
+            return image, label
 
 
 class ToTensor:
@@ -34,8 +36,9 @@ class ToTensor:
         image = self.toTensor(image)
         if label is not None:
             label = torch.from_numpy(np.array(label, dtype=np.int32)).long()
-        return (image, label)
-
+            return image, label
+        else:
+            return image
 
 class Blur:
     def __init__(self):
@@ -47,7 +50,10 @@ class Blur:
         ksize = ksize + 1 if ksize % 2 == 0 else ksize
         gaussian_blur = T.GaussianBlur(ksize, sigma)
         image = gaussian_blur(image)
-        return (image, label)
+        if label is not None:
+            return image, label
+        else:
+            return image
 
 class Resize():
     def __init__(self, size=(512, 512), interpolation='LINEAR'):
@@ -69,9 +75,12 @@ class Resize():
         image = self.resizeIm(image)
         if label is not None:
             label = self.resizeLabel(label)
-        return (image, label)
+            return image, label
+        else:
+            return image
 
-class HorizontalFlip():
+
+class HorizontalFlip:
     def __init__(self, prob=0.5):
         try:
             prob = float(prob)
@@ -88,6 +97,18 @@ class HorizontalFlip():
         return image, label
 
 
+class ColorJitter:
+    def __init__(self, brightness, contrast, saturation):
+        self.color_jitter = T.ColorJitter(brightness=brightness, contrast=contrast, saturation=saturation)
+
+    def __call__(self, image, label=None, *args, **kwargs):
+        image = self.color_jitter(image)
+        if label is not None:
+            return image, label
+        else:
+            return image
+
+
 class RandomCrop():
     def __init__(self, size=(512, 512)):
         if len(size) != 2:
@@ -99,9 +120,11 @@ class RandomCrop():
         image = T.functional.crop(image, t, l, h, w)
         if label is not None:
             label = T.functional.crop(label, t, l, h, w)
-        return (image, label)
+            return image, label
+        else:
+            return image
 
-class Rotate():
+class Rotate:
     def __init__(self,  angleMax=10):
         self.angleMax = angleMax
 
@@ -110,10 +133,12 @@ class Rotate():
         image = rotate(image, angle=angle)
         if label is not None:
             label = rotate(label, angle=angle)
-        return image, label
+            return image, label
+        else:
+            return image
 
 
-class Normalize():
+class Normalize:
     def __init__(self, mean, std):
         self.mean = mean
         self.std = std
@@ -121,44 +146,8 @@ class Normalize():
 
     def __call__(self, image, label=None):
         image = self.toNormalize(image)
-        return (image, label)
+        if label is not None:
+            return image, label
+        else:
+            return image
 
-
-
-    def _val_augmentation(self, image, label):
-        if self.resize:
-            self._resize(image, label)
-        if self.crop_size:
-            image, label = self._crop(image, label)
-        return image, label
-
-    def _augmentation(self, image, label):
-        # Scaling, we set the bigger to base size, and the smaller
-        # one is rescaled to maintain the same ratio, if we don't have any obj in the image, re-do the processing
-        # Rotate the image with an angle between -10 and 10
-        if self.rotate:
-            angle = random.randint(-10, 10)
-            image = rotate(image, angle=angle)
-            label = rotate(label, angle=angle)
-
-        if self.resize:
-            self._resize(image, label)
-
-        # Padding to return the correct crop size
-        if self.crop_size:
-            image, label = self._crop(image, label)
-
-        # Random H flip
-        if self.flip:
-            if random.random() > 0.5:
-                image = hflip(image)
-                label = hflip(label)
-
-        # Gaussian Blud (sigma between 0 and 1.5)
-        if self.blur:
-            sigma = random.random()
-            ksize = int(3.3 * sigma)
-            ksize = ksize + 1 if ksize % 2 == 0 else ksize
-            gaussian_blur = T.GaussianBlur(ksize, sigma)
-            image = gaussian_blur(image)
-        return image, label

utils/metric.py
@@ -30,21 +30,19 @@ def class_measurement(aInter, aPreds, aLabels):
     return mean_acc.item(), class_precision, class_recall
 
 
-def meanIoU(aInter, aPreds, aLabels, ignore_label=None):
-    if ignore_label is None and ignore_label in range(aPreds.shape[0]):
-        valid_class = aPreds.shape[0] - 1
-    else:
-        valid_class = aPreds.shape[0]
+def meanIoU(aInter, aPreds, aLabels):
     union = aPreds + aLabels - aInter
     class_iou = aInter / union
-    miou = class_iou.sum() / valid_class
+    miou = torch.mean(class_iou)
     return class_iou, miou.item()
 
 
-def calculate_area(prediction, target, classes):
-    one_hot_preds = F.one_hot(prediction, classes).transpose(1,3)
-    one_hot_targets = F.one_hot(target, classes).transpose(1,3)
-    intersection = (one_hot_preds*one_hot_targets).sum(dim=(0,2,3))
-    pPreds = one_hot_preds.sum(dim=(0, 2, 3))
-    pTarget = one_hot_targets.sum(dim=(0, 2, 3))
-    return intersection, pPreds, pTarget
+def calculate_hist(prediction, target, num_classes, ignore_labels=None):
+    if ignore_labels is None:
+        intersection = (prediction == target) * prediction
+    else:
+        intersection = (target != ignore_labels) * (prediction == target) * prediction
+    preds_intersection = torch.histc(intersection, bins=num_classes, min=0, max=num_classes-1)
+    preds_hist= torch.histc(prediction, bins=num_classes, min=0, max=num_classes-1)
+    target_hist = torch.histc(target, bins=num_classes, min=0, max=num_classes-1)
+    return preds_intersection, preds_hist, target_hist

val.py
@@ -19,24 +19,24 @@ def evaluate(model, eval_loader, num_classes, loss=None, lossCoef=None,
                 with torch.cuda.amp.autocast(enabled=True):
                     preds = model(data)
                     if loss is not None and lossCoef is not None:
-                        loss = loss_computation(logits_list=preds, labels=target, loss=loss, coef=lossCoef)
+                        loss = loss_computation(logits_list=preds, labels=target, criterion=loss, coef=lossCoef)
                         total_loss += loss.sum()
             else:
                 preds = model(data)
                 if loss is not None and lossCoef is not None:
-                    loss = loss_computation(logits_list=preds, labels=target, loss=loss, coef=lossCoef)
+                    loss = loss_computation(logits_list=preds, labels=target, criterion=loss, coef=lossCoef)
                     total_loss += loss.sum()
             if isinstance(preds, tuple):
                 preds = preds[0]
             pred = torch.argmax(preds, dim=1, keepdim=True)
-            inter, pPred, pTarget = calculate_area(pred, target, preds.shape()[1])
+            inter, pPred, pTarget = calculate_hist(pred, target, preds.shape[1], ignore_labels=ignore_labels)
 
             intersect += intersect
             pred_area += pPred
             label_area += pTarget
             tbar.update()
 
-    class_iou, miou = meanIoU(aInter=intersect, aPreds=pred_area, aLabels=label_area, ignore_label=ignore_labels)
+    class_iou, miou = meanIoU(aInter=intersect, aPreds=pred_area, aLabels=label_area)
     acc, class_precision, class_recall = class_measurement(aInter=intersect, aPreds=pred_area, aLabels=label_area)
     kap = kappa(aInter=intersect, aPreds=pred_area, aLabels=label_area)
 
@@ -47,5 +47,5 @@ def evaluate(model, eval_loader, num_classes, loss=None, lossCoef=None,
         'kappa': kap
     }
     if loss is not None and lossCoef is not None:
-        log['loss'] = total_loss / (len(eval_loader) * eval_loader.batch_size),
+        log['loss'] = total_loss / (len(eval_loader) * eval_loader.loader.batch_size),
     return log