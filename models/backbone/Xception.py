17117c9e018e1370219350583a9d6ebb6f6cad07
Florian <florian.scalvini@gmail.com>
2022-06-06 23:01:19 +0200

    add Xception

--- models/backbone/Xception.py
+++ models/backbone/Xception.py
@@ -17,7 +17,7 @@ class Xception(nn.Module):
 
 
 class SeperableConv(nn.Module):
-    def __init__(self, in_channels,out_channels,kernel_size=1,stride=1,padding=0,dilation=1,bias=False):
+    def __init__(self, in_channels,out_channels,kernel_size=3,stride=1,padding=0,dilation=1,bias=False):
         super(SeperableConv, self).__init__()
         self.conv1 = nn.Conv2d(in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels, bias=bias)
         self.pointwise = nn.Conv2d(in_channels, out_channels, 1, 1, 0, 1, 1, bias=bias)
@@ -27,14 +27,35 @@ class SeperableConv(nn.Module):
         y = self.pointwise(y)
         return y
 
+class MiddleFlow(nn.Module):
+    def __init__(self, channels, number=8):
+        super(MiddleFlow, self).__init__()
+        self.channels = channels
+        self.number = number
+        self.block = self._makeblock()
+
+    def _makeblock(self):
+        layers = []
+        for i in range(3):
+            layers.append(nn.Sequential(
+                nn.ReLU(),
+                SeperableConv(in_channels=self.channels, out_channels=self.channels),
+                nn.BatchNorm2d(num_features=self.channels)
+            ))
+
+    def forward(self, x):
+        for
+        return y
 
 class Block2RS_M(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Block2RS_M, self).__init__()
-        self.sep1 = SeperableConv(in_channels=)
-        self.bn1 = nn.BatchNorm2d(num_features=)
-        self.bn1 = nn.BatchNorm2d(num_features=)
+        self.sep1 = SeperableConv(in_channels=in_channels, out_channels=out_channels)
+        self.bn1 = nn.BatchNorm2d(num_features=out_channels)
+        self.sep2 = SeperableConv(in_channels=out_channels, out_channels=out_channels)
+        self.bn2 = nn.BatchNorm2d(num_features=out_channels)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2)
         self.relu = nn.ReLU()
+        self.conv = nn.C
 
     def forward(self, x):
